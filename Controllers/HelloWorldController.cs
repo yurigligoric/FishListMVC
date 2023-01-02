c46bdf3bfb3acc713b0059f6d941f192752c0bf4
yurigligoric <yurigligoric@gmail.com>
2023-01-02 18:30:48 +0800

    first commit

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FishListMVC.Models;
+
+namespace FishListMVC.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+
+        private static List<Fish> fishList = new List<Fish>();
+
+        public IActionResult Index()
+        {
+            return View(fishList);
+        }
+
+        public IActionResult Home()
+        {
+            return View(fishList);
+            //var fishVM = new Fish();
+            //fishVM.Name = "Vil";
+            //fishVM.Age = 9;
+
+            //return View(fishVM);
+        }
+
+        public IActionResult Create()
+        {
+            var fishVM = new Fish();
+
+            return View(fishVM);
+        }
+
+        public IActionResult CreateFish(Fish fish)
+        {
+            fishList.Add(fish);
+            //return View("Home");
+            return RedirectToAction(nameof(Home));
+        }
+    }
+}
---
+++ Models/Fish.cs
@@ -0,0 +1,9 @@
+﻿namespace FishListMVC.Models
+{
+    public class Fish
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+    }
+}
